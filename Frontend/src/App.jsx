efeecede1b17cdac84dff9d2fa556bf643bf3b23
kishumar <kishankumar20082000@gmail.com>
2025-09-27 19:33:09 +0530

    change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -67,7 +67,7 @@ function App() {
           </div>
         </div>
         <div className="right" 
-        
+        style={{ textItem:"center" }}
         >
           {loading ? (
             <div className="spinner"></div> // spinner while loading
