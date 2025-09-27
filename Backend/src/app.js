a72af55321a712fce47eb9367df7419313ceb288
kishumar <kishankumar20082000@gmail.com>
2025-09-27 15:21:31 +0530

    url change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,11 +2,21 @@ import express, { json } from 'express'
 import aiRoutes from './routes/ai.routes.js'
 import cors from 'cors'
 const app = express();
-app.use(cors())
+
 app.use(express.json())
 app.get('/', (req, res)=>{
     res.send("Hi am a code reviewer")
 } )
+
+const corsOptions = {
+  origin: [
+    'http://localhost:5173', // for local dev
+    'https://mern-code-reveiwer.vercel.app' // replace with your actual deployed frontend URL
+  ],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 
 app.use('/ai', aiRoutes)
 
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://mern-code-reveiwer.onrender.com/ai/get-review', { code })
     setReview(response.data)
   }
 
